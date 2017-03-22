6093692976b2b1c83a0ade9f6201ea3d31d18842
shuai yao <shuaiyao@shuaideMacBook-Pro.local>
2017-03-22 13:47:52 +0800

    小修改

--- app/app.jsx
+++ app/app.jsx
@@ -5,12 +5,18 @@
 import 'babel-polyfill';
 import 'bootstrap/scss/bootstrap.scss';
 import { Router, Route, Link } from 'react-router';
-
 import React from 'react';
 // ReactDOM 是一個 JS 的函式庫，這裡面包含很多 React 才能用的功能，而裡面幾乎都是處理 DOM 的功能。
 import ReactDOM from 'react-dom';
 import Deskmark from 'components/Deskmark';
-
 const app = document.createElement('div');
+// console.log(Deskmark);
+const Dashboard = React.createClass({
+    render() {
+        return <div>Welcome to the app!</div>
+    }
+})
 document.body.appendChild(app);
 ReactDOM.render(<Deskmark />, app);
+
+
